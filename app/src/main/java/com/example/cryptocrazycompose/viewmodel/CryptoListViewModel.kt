7db8f592de66462132bf96bb4668b5b183e656d1
furkan harmancı <rocklee34@hotmail.com>
2023-10-23 15:25:24 +0300

    feat: crypto data search func is added :sparkles:

--- app/src/main/java/com/example/cryptocrazycompose/viewmodel/CryptoListViewModel.kt
+++ app/src/main/java/com/example/cryptocrazycompose/viewmodel/CryptoListViewModel.kt
@@ -8,6 +8,7 @@ import com.example.cryptocrazycompose.model.CryptoListItem
 import com.example.cryptocrazycompose.repository.CryptoRepository
 import com.example.cryptocrazycompose.util.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,6 +20,49 @@ class CryptoListViewModel @Inject constructor(
     var cryptoList = mutableStateOf<List<CryptoListItem>>(listOf())
     var errorMessage = mutableStateOf("")
     var isLoading = mutableStateOf(false)
+
+    private var initialCryptoList = listOf<CryptoListItem>() // geçici bir liste bu
+    private var isSearchStarting = true
+
+    init {
+        loadCryptos()
+    }
+
+    fun searchCryptoList(query: String) {
+        val listToSearch = if (isSearchStarting) {
+            // arama başladıysa çekilen verinin değerini ver
+            cryptoList.value
+        } else {
+            // arama başlamadıysa geçici listeyi yapıştır
+            initialCryptoList
+        }
+
+        viewModelScope.launch(Dispatchers.Default) {
+            if (query.isEmpty()) {
+                // arama yapılıp silinmişsse veya arama yapılmamışsa boş kalmışsa crypto listesine geçici crypto listesini atarız
+                cryptoList.value = initialCryptoList
+                isSearchStarting = true
+                return@launch
+            }
+
+            // sonuçları aramanın başlayıp başlamadığına göre bir filtreye sokuyoruz.
+            // Arama başladıysa ve yazılan değerin harfi currency'de BARINIYORSA
+            val results = listToSearch.filter { cryptoListItem ->
+                cryptoListItem.currency.contains(query.trim(), true)
+                // ignore case -> büyük küçüğe duyarlı olmadan arama yapar
+                // trim ile boşlukları kaldırır
+            }
+
+            if (isSearchStarting) {
+                // arama başladıysa geçici listeye çektiğimiz veri listesinin değerini ata
+                initialCryptoList = cryptoList.value
+                // arama bitti
+                isSearchStarting = false
+            }
+            // çekilen veri listesine sonuçları yapıştır.
+            cryptoList.value = results
+        }
+    }
 
 
     fun loadCryptos() {
