22a8fa55c94311e4827026ed12d842036829ac0f
furkan harmancı <rocklee34@hotmail.com>
2023-10-24 13:23:02 +0300

    refactor: code line is deleted :package:

--- app/src/main/java/com/example/cryptocrazycompose/MainActivity.kt
+++ app/src/main/java/com/example/cryptocrazycompose/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : ComponentActivity() {
 
                 val navController = rememberNavController()
 
-
                 NavHost(
                     navController = navController,
                     startDestination = Routes.cryptoListScreenId) {
