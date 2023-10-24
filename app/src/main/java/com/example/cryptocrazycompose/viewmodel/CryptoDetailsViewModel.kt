e3a03e134b2254159ccf600e725849f2b2201747
furkan harmancı <rocklee34@hotmail.com>
2023-10-24 13:16:28 +0300

    feat: viewmodel function added :sparkles:

--- app/src/main/java/com/example/cryptocrazycompose/viewmodel/CryptoDetailsViewModel.kt
+++ app/src/main/java/com/example/cryptocrazycompose/viewmodel/CryptoDetailsViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.cryptocrazycompose.viewmodel
 
-class CryptoDetailsViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.cryptocrazycompose.model.Crypto
+import com.example.cryptocrazycompose.repository.CryptoRepository
+import com.example.cryptocrazycompose.util.Status
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CryptoDetailsViewModel @Inject constructor(
+    private val repository: CryptoRepository
+): ViewModel() {
+
+    suspend fun getCrypto(): Status<Crypto> {
+        return repository.getCrypto()
+    }
 }
